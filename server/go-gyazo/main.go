4e7879a19fe2c5389e2c53b248139b05d47a7b8d
mattn <mattn.jp@gmail.com>
2011-06-23 18:57:37 +0900

    add server

---
+++ server/go-gyazo/main.go
@@ -0,0 +1,32 @@
+package gyazo
+
+import (
+	"http"
+	"os"
+	"log"
+	"io/ioutil"
+	"github.com/hoisie/web.go"
+)
+
+func init() {
+	s := &web.Server{
+		Config: web.Config,
+		Logger: log.New(os.Stderr, "", log.LstdFlags),
+	}
+
+	s.Get("/", func(ctx *web.Context) {
+		b, _ := ioutil.ReadFile("index.html")
+		ctx.Write(b)
+	})
+	s.Post("/upload.cgi", func(ctx *web.Context) {
+		for k, f := range ctx.Request.Files {
+			s.Logger.Println(k)
+			if k == "imagedata" || f.Filename == "gyazo.com" {
+				ctx.Write(f.Data)
+				break
+			}
+		}
+	})
+
+	http.Handle("/", s)
+}
